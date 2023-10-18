a5e938f21a072488a9cf0176e30d5ea8de560124
SACRED GAMES 3.06 <106374961+YounisXyz@users.noreply.github.com>
2023-10-18 16:36:21 +0500

    Update Berlin.py

--- Berlin.py
+++ Berlin.py
@@ -1,8 +1,4 @@
 import os,platform
 os.system('git pull')
-xyz=platform.architecture()[0]
-if xyz=="32bit":
-    print('\033[1;91mSorry Your Device Is 32bit,This Tool Is Only For 64bit, Please Upgrade Your Phone ....')
-elif xyz=="64bit":
-    __import__("Berlin")
+import Berlin
  
