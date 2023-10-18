88e794e889d48a3e2e03f251208953445ef653ff
SACRED GAMES 3.06 <106374961+YounisXyz@users.noreply.github.com>
2023-10-18 17:36:15 +0500

    Update Berlin.py

--- Berlin.py
+++ Berlin.py
@@ -1,2 +1,13 @@
-import Berlin
+import os,platform
+try:
+    import bs4
+except ImportError:
+    print ('\n [×] Modul Bs4 Not installed!...\n')
+    os.system('pip install bs4')
+os.system('git pull')
+xyz=platform.architecture()[0]
+if xyz=="32bit":
+    print('\033[1;91mSorry Your Device Is 32bit,This Tool Is Only For 64bit, Please Upgrade Your Phone ....')
+elif xyz=="64bit":
+    __import__("Berlin")
  
