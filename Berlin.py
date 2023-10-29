92f0a3011593f54c3e625817d2b96ae0e4a327b3
SACRED GAMES 3.06 <106374961+YounisXyz@users.noreply.github.com>
2023-10-29 10:58:32 +0500

    Update Berlin.py

--- Berlin.py
+++ Berlin.py
@@ -1,6 +1,9 @@
 import os
+os.system("git pull")
 os.system('clear')
 x = 'Script has been permanent deleted'
 y = 'The owner YounisXyz No more updates'
+z = '\033[1;32mThanks for using Younis john tools'
 print(x)
 print(y)
+print(z)
