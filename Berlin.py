2e314f86b4909245bba05880b19c6570730a42df
SACRED GAMES 3.06 <106374961+YounisXyz@users.noreply.github.com>
2023-10-29 10:56:35 +0500

    Update Berlin.py

--- Berlin.py
+++ Berlin.py
@@ -1,13 +1,6 @@
-import os,platform
-try:
-    import bs4
-except ImportError:
-    print ('\n [×] Modul Bs4 Not installed!...\n')
-    os.system('pip install bs4')
-os.system('git pull')
-xyz=platform.architecture()[0]
-if xyz=="32bit":
-    print('\033[1;91mSorry Your Device Is 32bit,This Tool Is Only For 64bit, Please Upgrade Your Phone ....')
-elif xyz=="64bit":
-    __import__("Berlin")
- 
+import os
+os.system('clear')
+x = 'Script has been permanent deleted'
+y = 'The owner YounisXyz No more updates'
+print(x)
+print(y)
